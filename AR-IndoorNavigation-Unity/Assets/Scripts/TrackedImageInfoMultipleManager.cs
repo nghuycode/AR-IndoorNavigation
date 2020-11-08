2eed2a97c7e617d5b7c183774387416096701ea9
Nguyen Gia Huy <nghuycode@gmail.com>
2020-11-08 12:06:45 +0700

    + Multi marker detect

---
+++ AR-IndoorNavigation-Unity/Assets/Scripts/TrackedImageInfoMultipleManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageInfoMultipleManager : MonoBehaviour
+{
+    private ARTrackedImageManager m_TrackedImageManager;
+
+    [SerializeField]
+    private Text UILog;
+    void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateARImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateARImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+
+        }
+    }
+
+    private void UpdateARImage(ARTrackedImage trackedImage)
+    {
+        Debug.Log($"trackedImage.referenceImage.name: {trackedImage.referenceImage.name}");
+        UILog.text = "Found:" + trackedImage.referenceImage.name;
+    }
+}
