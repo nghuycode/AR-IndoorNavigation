7ff0023204d24d2fa5c7439bdf5ca06e45a8d16f
nghuycode <66381199+nghuycode@users.noreply.github.com>
2020-11-19 16:41:34 +0700

    Camera follow Player

---
+++ AR-IndoorNavigation-Unity/Assets/Scripts/CameraMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMap : MonoBehaviour
+{
+    public GameObject Player;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(Player.transform.position.x, this.transform.position.y, Player.transform.position.z);
+    }
+}
